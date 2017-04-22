4ccca692c0c62ba314d256d3b2b229824ae735bb
Zsombor Gegesy <gzsombor@gmail.com>
2017-04-22 19:48:27 +0200

    More final fields ...

--- generators/server/templates/src/main/java/package/service/mapper/_UserMapper.java
+++ generators/server/templates/src/main/java/package/service/mapper/_UserMapper.java
@@ -88,7 +88,7 @@ public class UserMapper {
         if (id == null) {
             return null;
         }
-        User user = new User();
+        final User user = new User();
         user.setId(id);
         return user;
     }
@@ -96,7 +96,7 @@ public class UserMapper {
 
     public Set<Authority> authoritiesFromStrings(Set<String> strings) {
         return strings.stream().map(string -> {
-            Authority auth = new Authority();
+            final Authority auth = new Authority();
             auth.setName(string);
             return auth;
         }).collect(Collectors.toSet());
